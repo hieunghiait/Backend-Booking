07211e45bc501c0ccdca834d7f9a88541ea7443a
Le Hieu Nghia <lehieunghia2402@gmail.com>
2023-03-27 19:08:48 +0700

    seccond-commit

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,18 @@
-let getHomePage = (req, res) => {
-    return res.render('homePage.ejs');
+import db from '../models/index';
+let getHomePage = async (req, res) => {
+    try{
+        let data = await db.User.findAll();
+        console.log('------------------------')
+        console.log(data)
+        console.log("------------------------");
+        return res.render('homePage.ejs', {
+            //truyen bien data ra view 
+            data: JSON.stringify(data)
+        });
+    }catch(e)
+    {
+        console.log(e)
+    }
 }
 let getAboutPage = (req, res) => {
     return res.render('test/about.ejs')
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngie from "./config/viewEngine";
 import initWebRoutes from "./route/web";
+import connectDB from "./config/connectDB";
 require("dotenv").config();
 let app = express();
 
@@ -10,6 +11,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 viewEngie(app);
 initWebRoutes(app);
+connectDB();
+
 let port = process.env.PORT || 6969;
 
 app.listen(port, () => {

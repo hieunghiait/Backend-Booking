4ebe7edd49f954acf8eb8540d35e9dc8b3dc8c2c
ThucDang <85912398+DangThuc202@users.noreply.github.com>
2023-05-16 09:31:54 +0700

    be1

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,115 +1,129 @@
 import db from "../models/index";
 /**
- * 
- * @param {*} limit 
- * @returns 
+ *
+ * @param {*} limit
+ * @returns
  */
 let getTopDoctorHomeService = (limit) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            let users = await db.User.findAll({
-                limit: limit,
-                where: { roleId: 'R2' },
-                //Sắp xếp theo ngày tạo giảm dần
-                order: [['createdAt', 'DESC']],
-                //Loại bỏ trường password
-                attributes: {
-                    exclude: ['password', 'image']
-                },
-                include: [
-                    { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
-                    { model: db.Allcode, as: 'genderData', attributes: ['valueEn, valueVi'] }
-                ],
-                raw: true,
-                nest: true
-            })
-            resolve({
-                errCode: 0,
-                data: users
-            })
-        } catch (error) {
-            reject(error)
-        }
-    })
-}
+  return new Promise(async (resolve, reject) => {
+    try {
+      console.log("abc", limit);
+      let users = await db.User.findAll({
+        limit: limit,
+        where: { roleId: "R2" },
+        //Sắp xếp theo ngày tạo giảm dần
+        order: [["createdAt", "DESC"]],
+        //Loại bỏ trường password
+        attributes: {
+          exclude: ["password", "image"],
+        },
+        include: [
+          {
+            model: db.Allcode,
+            as: "positionData",
+            attributes: ["valueEn", "valueVi"],
+          },
+          {
+            model: db.Allcode,
+            as: "genderData",
+            attributes: ["valueEn, valueVi"],
+          },
+        ],
+        raw: true,
+        nest: true,
+      });
+      resolve({
+        errCode: 0,
+        data: users,
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
 let getAllDoctorsService = () => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            let doctors = await db.User.findAll({
-                where: { roleId: 'R2' },
-                attributes: { exclude: ['password', 'image'] }
-            })
-            resolve({ errCode: 0, data: doctors })
-        } catch (error) { reject(error); }
-    })
-}
+  return new Promise(async (resolve, reject) => {
+    try {
+      let doctors = await db.User.findAll({
+        where: { roleId: "R2" },
+        attributes: { exclude: ["password", "image"] },
+      });
+      resolve({ errCode: 0, data: doctors });
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
 /**
- * 
- * @param {object} inputData 
- * @returns 
+ *
+ * @param {object} inputData
+ * @returns
  */
 let saveDetailInformationDoctor = (inputData) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            1
-            if (!inputData.id || !inputData.contentHTML || !inputData.contentMarkdown) {
-                resolve({
-                    errCode: 1,
-                    errMessage: 'Missing parameter',
-                })
-            } else {
-                await db.Markdown.create({
-                    contentHTML: inputData.contentHTML,
-                    contentMarkdown: inputData.contentMarkdown,
-                    description: inputData.description,
-                    doctorId: inputData.doctorId
-                })
-                resolve({
-                    errCode: 0,
-                    errMessage: 'Save information doctor sucessfully'
-                })
-            }
-        } catch (error) {
-            reject(error)
-        }
-    })
-}
+  return new Promise(async (resolve, reject) => {
+    try {
+      1;
+      if (
+        !inputData.id ||
+        !inputData.contentHTML ||
+        !inputData.contentMarkdown
+      ) {
+        resolve({
+          errCode: 1,
+          errMessage: "Missing parameter",
+        });
+      } else {
+        await db.Markdown.create({
+          contentHTML: inputData.contentHTML,
+          contentMarkdown: inputData.contentMarkdown,
+          description: inputData.description,
+          doctorId: inputData.doctorId,
+        });
+        resolve({
+          errCode: 0,
+          errMessage: "Save information doctor sucessfully",
+        });
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
 let getDetailDoctorById = (inputId) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            if (!inputId) {
-                resolve({
-                    errCode: 1,
-                    errMessage: 'Missing required parameter'
-                })
-            } else {
-                let data = await db.User.findOne({
-                    where: {
-                        id: inputId
-                    },
-                    attributes: {
-                        exclude: ['password', 'image']
-                    },
-                    include: [
-                        { model: db.Markdown }
-                    ],
-                    raw: true,
-                    nest: true
-                })
-                resolve({
-                    errCode: 0,
-                    data: data
-                })
-            }
-        } catch (error) {
-            console.log(error)
-            reject(error)
-        }
-    })
-}
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!inputId) {
+        resolve({
+          errCode: 1,
+          errMessage: "Missing required parameter",
+        });
+      } else {
+        let data = await db.User.findOne({
+          where: {
+            id: inputId,
+          },
+          attributes: {
+            exclude: ["password", "image"],
+          },
+          include: [{ model: db.Markdown }],
+          raw: true,
+          nest: true,
+        });
+        resolve({
+          errCode: 0,
+          data: data,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      reject(error);
+    }
+  });
+};
 module.exports = {
-    getTopDoctorHomeService: getTopDoctorHomeService,
-    getAllDoctorsService: getAllDoctorsService,
-    saveDetailInformationDoctor: saveDetailInformationDoctor,
-    getDetailDoctorById, getDetailDoctorById,
-}
+  getTopDoctorHomeService: getTopDoctorHomeService,
+  getAllDoctorsService: getAllDoctorsService,
+  saveDetailInformationDoctor: saveDetailInformationDoctor,
+  getDetailDoctorById,
+  getDetailDoctorById,
+};
